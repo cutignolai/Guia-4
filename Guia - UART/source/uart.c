3cefb84e3bf4400b107fa1234f38e5b32a30ab78
valen <vvenier@itba.edu.ar>
2022-09-10 11:50:08 -0300

    aarrancamos uart.c

---
+++ Guia - UART/source/uart.c
@@ -0,0 +1,100 @@
+/***************************************************************************//**
+  @file     UART.c
+  @brief    UART Driver for K64F. Non-Blocking and using FIFO feature
+  @author   Oli, Cuty y Micho
+ ******************************************************************************/
+
+#include "MK64F12.h"
+#include "uart.h"
+#include "gpio.h"
+#include "board.h"
+#include <stdbool.h>
+
+/*******************************************************************************
+ * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
+ ******************************************************************************/
+#define UART0_TX		DIO_20
+#define UART0_RX		DIO_25
+
+#define UART1_TX		DIO_21
+#define UART1_RX		DIO_26
+
+#define UART2_TX		DIO_22
+#define UART2_RX		DIO_27
+
+#define UART3_TX		DIO_23
+#define UART3_RX		DIO_28
+
+#define UART4_TX		DIO_24
+#define UART4_RX		DIO_29
+
+
+/*******************************************************************************
+ * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
+ ******************************************************************************/
+
+/*******************************************************************************
+ * VARIABLE PROTOTYPES WITH GLOBAL SCOPE
+ ******************************************************************************/
+
+/*******************************************************************************
+ * FUNCTION PROTOTYPES WITH GLOBAL SCOPE
+ ******************************************************************************/
+
+/**
+ * @brief Initialize UART driver
+ * @param id UART's number
+ * @param config UART's configuration (baudrate, parity, etc.)
+*/
+void uartInit (uint8_t id, uart_cfg_t config){
+
+}
+
+/**
+ * @brief Check if a new byte was received
+ * @param id UART's number
+ * @return A new byte has being received
+*/
+uint8_t uartIsRxMsg(uint8_t id){
+
+}
+
+/**
+ * @brief Check how many bytes were received
+ * @param id UART's number
+ * @return Quantity of received bytes
+*/
+uint8_t uartGetRxMsgLength(uint8_t id){
+
+}
+
+/**
+ * @brief Read a received message. Non-Blocking
+ * @param id UART's number
+ * @param msg Buffer to paste the received bytes
+ * @param cant Desired quantity of bytes to be pasted
+ * @return Real quantity of pasted bytes
+*/
+uint8_t uartReadMsg(uint8_t id, char* msg, uint8_t cant){
+
+}
+
+/**
+ * @brief Write a message to be transmitted. Non-Blocking
+ * @param id UART's number
+ * @param msg Buffer with the bytes to be transfered
+ * @param cant Desired quantity of bytes to be transfered
+ * @return Real quantity of bytes to be transfered
+*/
+uint8_t uartWriteMsg(uint8_t id, const char* msg, uint8_t cant){
+
+}
+
+/**
+ * @brief Check if all bytes were transfered
+ * @param id UART's number
+ * @return All bytes were transfered
+*/
+uint8_t uartIsTxMsgComplete(uint8_t id){
+    
+}
